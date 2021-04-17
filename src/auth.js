f8e2b4620ef66af69d5e2d6404d17d97518eb8d7
ArthurLima <arthurdeolima@gmail.com>
2021-04-16 21:29:33 -0300

    Arrumando erro do subscription

--- src/auth.js
+++ src/auth.js
@@ -1,27 +1,30 @@
 import jwt from "jsonwebtoken";
 
 export default (request) => {
-  
-  const authHeader = request.headers.authorization;
-  const [scheme, token] = authHeader
-  ? authHeader.split(" ")
-  : [undefined, undefined];
-  
-  if (!token || token === null || !/^Bearer$/i.test(scheme))
-  return { authenticated: false };
-  
-  let data;
-
-  try {
-    data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  } catch (err) {
-    console.log(err)
+  if (request){
+    
+    const authHeader = request.headers.authorization;
+    const [scheme, token] = authHeader
+    ? authHeader.split(" ")
+    : [undefined, undefined];
+    
+    if (!token || token === null || !/^Bearer$/i.test(scheme))
     return { authenticated: false };
-  }
+    
+    let data;
   
-  // in case any error found
-  if (!!!data) return { authenticated: false };
-
-  // token decoded successfully, and extracted data
-  return { authenticated: true, ...data };
+    try {
+      data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (err) {
+      console.log(err)
+      return { authenticated: false };
+    }
+    
+    // in case any error found
+    if (!!!data) return { authenticated: false };
+  
+    // token decoded successfully, and extracted data
+    return { authenticated: true, ...data };
+  }
+  return { authenticated: false };
 };
