d9a90eb157e3c76d392208ddfd062b1cff5552fd
Arthur Lima <arthurdeolima@gmai.com>
2021-06-17 19:43:56 -0300

    Feat: importacao de ChangeLog correta

--- src/models/index.js
+++ src/models/index.js
@@ -4,4 +4,4 @@ export { default as SessionModel } from "./Session";
 export { default as RoleModel } from "./Role";
 export { default as EventModel } from "./Event";
 export { default as JustificativeModel } from "./Justificative";
-export { default as ChangelogModel } from "./Changelog";
+export { default as ChangelogModel } from "./ChangesLog";
