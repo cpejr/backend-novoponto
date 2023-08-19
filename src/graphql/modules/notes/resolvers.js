1dc22c71c001e71d20d48b16c9cf4764f5513679
M-Muniz <matheus.muniz.soares.mms@gmail.com>
2023-08-19 19:31:39 -0300

    Rodando mas n funciona o playground

--- src/graphql/modules/notes/resolvers.js
+++ src/graphql/modules/notes/resolvers.js
@@ -4,20 +4,13 @@ import jwt from "jsonwebtoken";
 import { NoteModel, StudentModel } from "../../../models";
 
 export default {
-  Note: {
-    student: ({ student, studentId }) => {
-      if(student) return student;
-      else return StudentModel.findById(studentId);
-    },
-  },
-
   Query: {
-    Notes: (_, { studentId, noteId, text }) => NoteModel.find( {studentId}, {noteId}, {text} ),
+    note: (_, { _id }) => NoteModel.findById(_id),
   },
 
   Mutation: {
-    sendNote: async (_, { data }) => NoteModel.create(data),
-    deleteNote: async (_, { _id }) => NoteModel.findByIdAndDelete(_id),
+    createNote: (_, { data }) => NoteModel.create(data),
+    deleteNote: async (_, { _id }) => !!(await NoteModel.findByIdAndDelete(_id)),
     updateNote: (_, { noteId, data }) => NoteModel.findOneAndUpdate({ _id: noteId }, data, {new: true}),
   },
 };
--- src/graphql/modules/students/resolvers.js
+++ src/graphql/modules/students/resolvers.js
@@ -4,15 +4,14 @@ import jwt from "jsonwebtoken";
 import { StudentModel, NoteModel } from "../../../models";
 
 export default {
-    Note: {
-  
-    },
-  
     Query: {
-  
+      students: () => StudentModel.find(),
+      student: (_, { _id }) => StudentModel.findById(_id), 
     },
   
     Mutation: {
-      
+      createStudent: (_, { data }) => StudentModel.create(data),
+      deleteStudent: async (_, { _id }) => !!(await StudentModel.findByIdAndDelete(_id)),
+      updateStudent: (_, { studentId, data }) => StudentModel.findOneAndUpdate({ _id: studentId }, data, {new: true})
     }
   }
