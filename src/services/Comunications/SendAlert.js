8e4d4a976043b3ddcb89404bcfba7532e2380a96
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2024-10-14 01:28:32 -0300

    taking out secrets

--- src/services/Comunications/SendAlert.js
+++ src/services/Comunications/SendAlert.js
@@ -1,7 +1,6 @@
 import twilio from "twilio";
 
-const accountSid = "AC8375911bb09f2383e89af8b56f8f9b58";
-const authToken = "d8ef96ab64555104fdb00e047082eced";
+
 const client = require("twilio")(accountSid, authToken);
 
 export const sendAlert = (phoneNumber, message) => {
