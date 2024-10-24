c424d3bd2dc6292ebbe75d5bbb95ae3fcbd4e56a
Otavio <otavioaugustobr@gmail.com>
2024-10-23 23:03:22 -0300

    fix: deleting comments

--- src/utils/usersList.js
+++ src/utils/usersList.js
@@ -13,7 +13,7 @@ async function usersList({ sheetID, userName }) {
 
     const csvText = await response.text();
     const data = parseCSV(csvText);
-    const secondColumn = data.map((row) => row[1]); // Pega a segunda coluna
+    const secondColumn = data.map((row) => row[1]);
 
     for (let i = 1; i < secondColumn.length; i++) {
       if (secondColumn[i].includes(userName)) {
@@ -28,7 +28,7 @@ async function usersList({ sheetID, userName }) {
 
   function parseCSV(text) {
     const rows = text.split("\n"); // Divide o texto em linhas
-    return rows.map((row) => row.split(",").map((cell) => cell.trim())); // Divide cada linha em células
+    return rows.map((row) => row.split(",").map((cell) => cell.trim()));
   }
 }
 
