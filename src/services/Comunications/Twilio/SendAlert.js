8b291c3b19f99c93c2cb0b874142b3b1a1269c8c
Gustavo Rezende <gustavo.olicardoso07@gmail.com>
2024-10-29 16:13:34 -0300

    feat: Twilio Folder + .env variables

--- src/services/Comunications/Twilio/SendAlert.js
+++ src/services/Comunications/Twilio/SendAlert.js
@@ -1,10 +1,14 @@
-const client = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
+const Account = process.env.ACCOUNTSID;
+const Token = process.env.AUTHTOKEN;
+TwilioNumber = process.env.TWILIONUMBER;
+
+const client = require("twilio")(Account, Token);
 
 export const sendAlert = (phoneNumber, message) => {
   client.messages
     .create({
       body: message,
-      from: process.env.TWILIONUMBER,
+      from: TwilioNumber,
       to: phoneNumber,
     })
     .then((message) => console.log("Alert sent: " + message.sid))
