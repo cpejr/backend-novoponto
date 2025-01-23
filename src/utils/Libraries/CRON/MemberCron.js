2f58cc4c4e6a89554d5eabf125233ef9f39b48fa
ZeL-ucas <zelucaspomps15@gmail.com>
2025-01-23 16:34:58 -0300

    change to sunday

--- src/utils/Libraries/CRON/MemberCron.js
+++ src/utils/Libraries/CRON/MemberCron.js
@@ -8,7 +8,6 @@ const  checkMemberHours  = async () => {
     
     const members = await MemberModel.find().populate({path:"roleId"})
     const manager = members.find((member)=> member.roleId.name == "Gerente de Clima e Membros")
-    console.log(manager)
     if (!manager) {
         return;
     }
@@ -84,7 +83,7 @@ const hoursSum = (sessions, additionalHours) => {
 };
 
 export const startMemberCron = () => {
-    cron.schedule("0 * * * * *", () => {
+    cron.schedule("0 0 23 * * 0", () => {
       checkMemberHours()
     });
   };
