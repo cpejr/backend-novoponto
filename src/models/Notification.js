2ed3f8ac28653a41bd6692d6f1c5d131fa9af3b2
ZeL-ucas <zelucaspomps15@gmail.com>
2025-04-10 10:56:56 -0300

    seeing notificaitons

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -4,8 +4,9 @@ const NotificationSchema = new mongoose.Schema({
   text: { type: String, required: true },
   link: { type: String, required: true },
   linkValidation: { type: String },
+  expiresAt: { type: Date, default: null },
 });
-
+NotificationSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
 const NotificationModel = mongoose.model("Notification", NotificationSchema);
 
 export default NotificationModel;
